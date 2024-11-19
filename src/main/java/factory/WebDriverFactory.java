df9f4339b872212665b0103ba76302f3bc6550e9
Roman Kondratev <kondratev@auction-house.ru>
2024-11-19 22:46:16 +0300

    Доработки по заданию #3

--- src/main/java/factory/WebDriverFactory.java
+++ src/main/java/factory/WebDriverFactory.java
@@ -1,33 +1,36 @@
 package factory;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebDriverFactory {
     private WebDriver driver;
 
     public WebDriver createDriver(String browser, BrowserMode mode) {
         ModeSettings modeSettings = new ModeSettings(mode);
-
-        switch (browser.toLowerCase()) {
-            case "chrome":
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions chromeOptions = modeSettings.getChromeOptions();
-                driver = new org.openqa.selenium.chrome.ChromeDriver(chromeOptions);
-                break;
-
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                FirefoxOptions firefoxOptions = modeSettings.getFirefoxOptions();
-                driver = new org.openqa.selenium.firefox.FirefoxDriver(firefoxOptions);
-                break;
-
-            default:
-                throw new IllegalArgumentException("Browser не поддерживается: " + browser);
+        String seleniumGridUrl = System.getenv("SELENIUM_GRID_URL");
+        if (seleniumGridUrl == null || seleniumGridUrl.isEmpty()) {
+            throw new IllegalArgumentException("SELENIUM_GRID_URL не указан.");
         }
-
+        try {
+            switch (browser.toLowerCase()) {
+                case "chrome":
+                    ChromeOptions chromeOptions = modeSettings.getChromeOptions();
+                    driver = new RemoteWebDriver(new URL(seleniumGridUrl), chromeOptions);
+                    break;
+                case "firefox":
+                    FirefoxOptions firefoxOptions = modeSettings.getFirefoxOptions();
+                    driver = new RemoteWebDriver(new URL(seleniumGridUrl), firefoxOptions);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Browser не поддерживается: " + browser);
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Некорректный URL для Selenium Grid: "+ seleniumGridUrl, e);
+        }
         return driver;
     }
 }
